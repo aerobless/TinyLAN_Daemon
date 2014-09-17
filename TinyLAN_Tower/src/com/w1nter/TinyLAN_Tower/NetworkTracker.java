18b622c689a795bc6d12812c9ccb197afa727a59
TheoWinter <theo.winter@gmail.com>
2014-09-17 21:00:58 +0200

    setup basic socket server

--- TinyLAN_Tower/src/com/w1nter/TinyLAN_Tower/NetworkTracker.java
+++ TinyLAN_Tower/src/com/w1nter/TinyLAN_Tower/NetworkTracker.java
@@ -1,15 +1,33 @@
 package com.w1nter.TinyLAN_Tower;
 
-public class NetworkTracker extends Thread {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
-	private void sleepInSeconds(int sec) {
+import com.w1nter.TinyLAN_Beacon.DataObjects.NetworkReport;
+
+public class NetworkTracker extends Thread {
+	ArrayList<NetworkReport> reportList;
+
+	@Override
+	public void run() {
 		try {
-			Thread.sleep(sec*1000);
-		} catch (InterruptedException e) {
-			log("Interrupted while trying to sleep", e);
+		    ServerSocket server = new ServerSocket(8989);
+			
+		    while (true) {
+		        Socket client = server.accept();
+	
+		        //answer to client
+		        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+		        out.println("333 OK");
+		    }
+		} catch (IOException e) {
+			log("IOException in NetworkTracker Server", e);
 		}
 	}
-	
+
 	public void log(String s){
 		System.out.println(s);
 	}
@@ -17,14 +35,5 @@ public class NetworkTracker extends Thread {
 	public void log(String s, Exception e){
 		System.out.println(s+ e.getMessage());
 	}
-
-	@Override
-	public void run() {
-		while(true){
-			log("tracking.. ");
-			sleepInSeconds(5);
-		}
-	}
-
 
 }
